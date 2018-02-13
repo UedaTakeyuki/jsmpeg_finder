34f9be12053cf28c3999f3d60f43132a09e9ede0
Takeyuki UEDA <ueda@atelierdueda.com>
2018-02-13 11:05:05 +0900

    戻る

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 // シェルスクリプト
-define("STOP_WEBSOCKET_RELAY", 'pkill -9 -f websocket-relay.sh');
-define("START_WEBSOCKET_RELAY", '/var/www/html/jsmpeg_finder/websocket-relay.sh &');
-define("STOP_MPEG1PUSH", 'pkill -9 -f mpeg1push.py');
-define("START_MPEG1PUSH", 'python /var/www/html/jsmpeg_finder/mpeg1push.py &');
+define("STOP_WEBSOCKET_RELAY", 'sudo pkill -9 -f websocket-relay.sh');
+define("START_WEBSOCKET_RELAY", 'sudo /var/www/html/jsmpeg_finder/websocket-relay.sh &');
+define("STOP_MPEG1PUSH", 'sudo pkill -9 -f mpeg1push.py');
+define("START_MPEG1PUSH", 'sudo python /var/www/html/jsmpeg_finder/mpeg1push.py &');
 
 $output=shell_exec(STOP_WEBSOCKET_RELAY);
 $output=shell_exec(START_WEBSOCKET_RELAY);
@@ -21,9 +21,24 @@ $output=shell_exec(START_MPEG1PUSH);
 			text-align: center;
 		}
 	</style>
+	<link rel="stylesheet" href="../jquery.mobile-1.3.2.min.css" />
+  <script src="../jquery-1.11.3.min.js "></script>
+  <script src="../custom-scripting.js"></script>
+  <script src="../jquery.mobile-1.3.2.min.js"></script>
+
 	
 </head>
 <body>
+
+<div data-role="page">
+	  
+<div data-role="header" data-position="fixed">
+    <h1><?= TITLE ?></h1>
+    <a href="../reset.php">戻る</a>
+</div>
+
+<div data-role="content" data-theme="c" class="no-cache">
+
 	<canvas id="video-canvas"></canvas>
 	<script type="text/javascript" src="jsmpeg/jsmpeg.min.js"></script>
 	<script type="text/javascript">
@@ -31,5 +46,11 @@ $output=shell_exec(START_MPEG1PUSH);
 		var url = 'ws://'+document.location.hostname+':8082/';
 		var player = new JSMpeg.Player(url, {canvas: canvas});
 	</script>
+</div>
+<div data-role="footer" data-position="fixed" data-disable-page-zoom="false">
+    <h4>© Atelier UEDA <img src="../favicon.ico"></h4>
+</div>
+</div> <!-- page -->
+
 </body>
 </html>
